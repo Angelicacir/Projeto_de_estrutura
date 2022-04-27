102f618abf12c930a197add7ce959821563eec12
Angélica Ciriaco <angelicaciriaco64@gmail.com>
2022-04-27 08:59:59 -0300

    arquivo aluno.c
    Professor no código aluno.c estar apresentando um erro e não estamos conseguindo identificar, o senhor poderia explicar esse erro? no caso achamos que seja na biblioteca.

---
+++ projeto/aluno2704/aluno.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct aluno{
+    int id;
+    char *nome;
+    float nota;
+}*Aluno;
+
+Aluno criarAluno(int a, char* b, unsigned short c){
+    Aluno A =(Aluno) malloc(sizeof(struct aluno));
+    A->id = a;
+    A->nome = b;
+    A->nota = c;
+    return A;
+}
+
+void modificarNota(Aluno A, float novaNota){
+    A->nota = novaNota;
+}
