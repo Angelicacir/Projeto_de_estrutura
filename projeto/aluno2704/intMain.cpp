427bf50a7ba17ebb8bcfad8a1290e6c5232e5cf1
Angélica Ciriaco <angelicaciriaco64@gmail.com>
2022-04-27 10:18:22 -0300

    atualização do arquivo intMain.cpp
    antes o arquivo era chamando de aluna.c agora ficou com o nome de intMain.cpp

---
+++ projeto/aluno2704/intMain.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stack>
+#include "aluno.cpp"
+
+using namespace std;
+
+typedef struct node {
+    Aluno A;
+    struct node* esq;
+    struct node* dir;
+}*No;
+
+typedef No noRaiz;
+
+noRaiz criarArvore();
+int inserir(noRaiz, Aluno);
+int inserirEmLote(noRaiz, noRaiz);
+int remover(noRaiz, Aluno a);
+Aluno buscar(noRaiz, Aluno a);
+void deletarArvore(noRaiz);
+
+//Recursivo
+void preordemRec(noRaiz);
+void inordemRec(noRaiz);
+void posordemRec(noRaiz);
+void nivelRec(noRaiz);
+
+//Iterativo
+void preordemIter(noRaiz);
+void inordemIter(noRaiz);
+void posordemIter(noRaiz);
+void nivelIter(noRaiz);
+
+int altura(noRaiz);
+int alturaDoNo(noRaiz);
+void valoresEntre(int idA, int idB);
+
+int tempoDeBusca();
+int tempoDeRemocao();
+int tempoDeInsercao();
+
+
+void inordemIter(noRaiz n){
+    stack<No*> elements;
+    No auxNo = n;
+    while (auxNo != NULL || !elements.empty()){
+        while(auxNo != NULL){
+            elements.push(auxNo);
+            auxNo = auxNo -> esq;
+        }
+        cout << elements.top() -> id << endl;
+        auxNo = elements.top() -> dir;
+        elements.pop();
+    }
+
+}
+
+int main( ){
+    Aluno K = criarAluno(10, "Kennedy Lopes\0", 9.);
+    No raiz = malloc(sizeof(struct node));
+    raiz->esq = 0;
+    raiz->dir = 0;
+    raiz->A = K;
+}
