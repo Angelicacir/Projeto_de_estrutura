d088f57dce8a2dc860ca04db72af5c3a7e1a5e88
Angélica Ciriaco <angelicaciriaco64@gmail.com>
2022-04-29 19:18:14 -0300

    resposta final

--- projeto/aluno2704/intMain.cpp
+++ projeto/aluno2704/intMain.cpp
@@ -40,51 +40,6 @@ int tempoDeRemocao();
 int tempoDeInsercao();
 
 
-void inordemIter(noRaiz n){…
-[22:27, 27/04/2022] Angelo Ufersa: RODOU
-[22:27, 27/04/2022] Angelo Ufersa: Mas aconteceu nada
-[22:32, 27/04/2022] Angelo Ufersa: #include <iostream>
-#include <stack>
-#include "aluno.cpp"
-
-using namespace std;
-
-typedef struct node {
-    Aluno A;
-    struct node* esq;
-    struct node* dir;
-}*No;
-
-typedef No noRaiz;
-
-noRaiz criarArvore();
-int inserir(noRaiz, Aluno);
-int inserirEmLote(noRaiz, noRaiz);
-int remover(noRaiz, Aluno a);
-Aluno buscar(noRaiz, Aluno a);
-void deletarArvore(noRaiz);
-
-//Recursivo
-void preordemRec(noRaiz);
-void inordemRec(noRaiz);
-void posordemRec(noRaiz);
-void nivelRec(noRaiz);
-
-//Iterativo
-void preordemIter(noRaiz);
-void inordemIter(noRaiz);
-void posordemIter(noRaiz);
-void nivelIter(noRaiz);
-
-int altura(noRaiz);
-int alturaDoNo(noRaiz);
-void valoresEntre(int idA, int idB);
-
-int tempoDeBusca();
-int tempoDeRemocao();
-int tempoDeInsercao();
-
-
 void inordemIter(noRaiz n){
     stack<No> elements;
     No auxNo = n;
